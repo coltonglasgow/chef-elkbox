510a87898e5f62b61aab24607fa684eb015ac84e
Colton <colton.glasgow@outlook.com>
2016-12-21 15:34:53 -0500

    Add files via upload

--- elkbox.rb
+++ elkbox.rb
@@ -1,12 +1,18 @@
-#Champtc 2016 
+#Champtc 2016
 
 #system(xterm -e *) used for ruby scripts and unfamiliar (to me) chef commands
 
 #yum update
-system('sudo yum update -y')
+execute 'yum update' do
+ action :run
+ command 'yum update -y'
+end
 
 #xterm for popout terminals
-system('sudo yum install xterm -y')
+execute 'xterm' do
+ action :run
+ command 'yum install xterm'
+end
 
 #ruby
 yum_package 'ruby' do
@@ -19,46 +25,85 @@ end
 yum_package 'net-tools' do
 end
 
-yum_package 'nano' do 
+yum_package 'nano' do
+end
+
+#change user pw
+execute 'change user (vagrant) password' do
+ action :run
+ command 'xterm -e passwd vagrant'
+end
+
+#change root pw
+execute 'change root password' do
+ action :run
+ command 'xterm -e passwd'
 end
 
 #static IP (in progress)
-system('xterm -e ruby /etc/chef/staticip.rb')
-
-#java
-system('sudo wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-linux-x64.rpm"')
-
-system('sudo yum localinstall jdk-8u112-linux-x64.rpm -y')
-
-system('sudo rm /jdk-8u112-linux-x64.rpm*')
-
-#elasticsearch
-system('sudo rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch')
-
-yum_repository "elasticsearch" do
-	description 'Elasticsearch repository for 5.x packages'
-	baseurl 'https://artifacts.elastic.co/packages/5.x/yum'
-	gpgkey 'https://artifacts.elastic.co/GPG-KEY-elasticsearch'
-	action :create
+execute 'static ip setup' do
+ action :run
+ command 'xterm -e ruby /etc/chef/staticip.rb'
 end
 
-system('/bin/ssytemctl daemon-reload')
-system('/bin/ssytemctl enable elastic.service')
+#java
+execute 'java get' do
+ action :run
+ command 'sudo wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-linux-x64.rpm"'
+end
+
+execute 'java install' do
+ action :run
+ command 'sudo yum localinstall jdk-8u112-linux-x64.rpm -y'
+end
+
+execute 'rm old jkd*' do
+ action :run
+ command 'sudo rm /jdk-8u112-linux-x64.rpm*'
+end
+
+#elasticsearch
+execute 'import elasticsearch' do
+ action :run
+ command 'sudo rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch'
+end
+
+yum_repository "elasticsearch" do
+    description 'Elasticsearch repository for 5.x packages'
+    baseurl 'https://artifacts.elastic.co/packages/5.x/yum'
+    gpgkey 'https://artifacts.elastic.co/GPG-KEY-elasticsearch'
+    action :create
+end
+
+execute 'reload services' do
+ action :run
+ command '/bin/systemctl daemon-reload'
+end
+execute 'enable elastic search service' do
+ action :run
+ command '/bin/systemctl enable elasticsearch.service'
+end
 
 #kibana
 yum_repository "kibana" do
-	description 'Kibana repository for 5.x packages'
-	baseurl 'https://artifacts.elastic.co/packages/5.x/yum'
-	gpgkey 'https://artifacts.elastic.co/GPG-KEY-elasticsearch'
-	action :create
+    description 'Kibana repository for 5.x packages'
+    baseurl 'https://artifacts.elastic.co/packages/5.x/yum'
+    gpgkey 'https://artifacts.elastic.co/GPG-KEY-elasticsearch'
+    action :create
 end
 
 yum_package 'kibana' do
 end
 
-	#start service on startup
-system('sudo /bin/systemctl daemon-reload')
-system('sudo /bin/systemctl enable kibana.service')
+    #start service on startup
+execute 'reload services' do
+ action :run
+ command '/bin/systemctl daemon-reload'
+end
+execute 'enable elastic search service' do
+ action :run
+ command '/bin/systemctl enable kibana.service'
+end
 
 #epel-release
 yum_package 'epel-release' do
@@ -68,78 +113,115 @@ end
 yum_package 'nginx' do
 end
 
-	#server block
-system('xterm -e ruby /etc/chef/kibanaconf.rb')
+    #server block
+execute 'create kibana.conf' do
+ action :run
+ command 'xterm -e ruby /etc/chef/kibanaconf.rb'
+end
 
 #httpd-tools
 yum_package 'httpd-tools' do
 end
 
 #http password
-system('xterm -e ruby /etc/chef/kibanauser.rb')
+execute 'configure kibana htaccess' do
+ action :run
+ command 'xterm -e ruby /etc/chef/kibanauser.rb'
+end
 
 #logstash
 yum_repository "logstash" do
-	description 'Elastic repository for 5.x packages'
-	baseurl 'https://artifacts.elastic.co/packages/5.x/yum'
-	gpgkey 'https://artifacts.elastic.co/GPG-KEY-elasticsearch'
-	action :create
+    description 'Elastic repository for 5.x packages'
+    baseurl 'https://artifacts.elastic.co/packages/5.x/yum'
+    gpgkey 'https://artifacts.elastic.co/GPG-KEY-elasticsearch'
+    action :create
 end
 
 yum_package 'logstash' do
 end
 
-	#start service on startup
-system('sudo /bin/systemctl daemon-reload')
-system('sudo /bin/systemctl enable logstash.service')
+    #start service on startup
+execute 'reload services' do
+ action :run
+ command '/bin/systemctl daemon-reload'
+end
+execute 'enable logstash search service' do
+ action :run
+ command '/bin/systemctl enable logstash.service'
+end
 
-	#plugins
-system('sudo /usr/share/logstash/bin/logstash-plugin install logstash-output-stomp')
-system('sudo /usr/share/logstash/bin/logstash-plugin install logstash-filter-tld')
-system('sudo /usr/share/logstash/bin/logstash-plugin update')
+    #plugins
+execute 'logstash-output-stomp plugin' do
+ action :run
+ command 'sudo /usr/share/logstash/bin/logstash-plugin install logstash-output-stomp'
+end
+execute 'logstash-filter-tld plugin' do
+ action :run
+ command 'sudo /usr/share/logstash/bin/logstash-plugin install logstash-filter-tld'
+end
+execute 'plugin update' do
+ action :run
+ command 'sudo /usr/share/logstash/bin/logstash-plugin update'
+end
 
-#geoip 
+#geoip
 directory '/var/local/geoip' do
  action :create
 end
-	#update
-system('/etc/logstash/geo-update.bash > /dev/null 2>&1')
+    #update
+#execute 'geoip update' do
+# action :run
+# command '/etc/logstash/geo-update.bash > /dev/null 2>&1'
+#end
 
 #disable selinux
 require 'tempfile'
  
-		def file_edit(filename, regexp, replacement)
-			Tempfile.open(".#{File.basename(filename)}", File.dirname(filename)) do |tempfile|
-				File.open(filename).each do |line|
-				tempfile.puts line .gsub(regexp, replacement)
-			end
-			tempfile.fdatasync
-			tempfile.close
-			stat = File.stat(filename)
-			FileUtils.chown stat.uid, stat.gid, tempfile.path
-			FileUtils.chmod stat.mode, tempfile.path
-			FileUtils.mv tempfile.path, filename
-			end
-		end
+        def file_edit(filename, regexp, replacement)
+            Tempfile.open(".#{File.basename(filename)}", File.dirname(filename)) do |tempfile|
+                File.open(filename).each do |line|
+                tempfile.puts line .gsub(regexp, replacement)
+            end
+            tempfile.fdatasync
+            tempfile.close
+            stat = File.stat(filename)
+            FileUtils.chown stat.uid, stat.gid, tempfile.path
+            FileUtils.chmod stat.mode, tempfile.path
+            FileUtils.mv tempfile.path, filename
+            end
+        end
 
-		file_edit('/etc/sysconfig/selinux', 'enforcing', 'disabled')
+        file_edit('/etc/sysconfig/selinux', 'enforcing', 'disabled')
 
-#disable firewall 
-system('sudo systemctl stop firewalld')
-system('sudo systemctl disable firewalld')
+#disable firewall
+execute 'stop firewalld' do
+ action :run
+ command 'systemctl stop firewalld'
+end
+execute 'disable firewalld' do
+ action :run
+ command 'systemctl disable firewalld'
+end
+
 
 #activemq
-system('chmod +x /etc/chef/activemq.sh')
-system('./etc/chef/activemq.sh')
+execute 'makes activemq.sh executable' do
+ action :run
+ command 'chmod +x /etc/chef/activemq.sh'
+end
+execute 'installs activemq' do
+ action :run
+ command './etc/chef/activemq.sh'
+end
 
 
 #CRON JOB
 #geoip update
 cron 'geoip update' do
-	minute '0'
-	hour '12'
-	day '*'
-	month '*'
-	weekday '3'
-	command '/etc/logstash/geo-update.bash > /dev/null 2>&1'
+    minute '0'
+    hour '12'
+    day '*'
+    month '*'
+    weekday '3'
+    command '/etc/logstash/geo-update.bash > /dev/null 2>&1'
 end
